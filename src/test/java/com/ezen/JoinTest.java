632ec4fdc23332c1bb1ab702dd1483eb4b9df358
Jeong <ver05@naver.com>
2022-07-18 15:08:58 +0900

    회원기능 가입구현 완료

---
+++ src/test/java/com/ezen/JoinTest.java
@@ -0,0 +1,18 @@
+package com.ezen;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoinTest{
+	@Test
+public void joinTest(){
+	System.out.println("테스트이빈다");
+}
+
+}
